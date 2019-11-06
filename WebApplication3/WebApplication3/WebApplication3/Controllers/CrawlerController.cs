051f7312bd89c005d4b4e86f3f5422d9fcff350a
Jayaraj Alagu Ponniah <40276417+Sama-raj@users.noreply.github.com>
2019-11-06 20:19:26 +1100

    Add files via upload
    This is the rest API which connects the crawler and the bing web search

---
+++ WebApplication3/WebApplication3/WebApplication3/Controllers/CrawlerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrawlerController : ControllerBase
+    {
+
+        [HttpOptions]
+        public bool RunCrawler(string urlfilenames)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "D:\\rmit\\project\\AzureSearchCrawler\\AzureSearchCrawler\\bin\\Release\\AzureSearchCrawler.exe";
+
+                process.StartInfo.Arguments = urlfilenames;
+                process.Start();
+                process.WaitForExit();
+                int exitCode = process.ExitCode;
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
